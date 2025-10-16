292f4fba92e8eff5c5f608ecd10679fe359ccb6a
yusa-a <yusa-a@weseek.co.jp>
2025-10-16 05:42:39 +0000

    change val name

--- apps/app/src/server/util/createApiRouter.ts
+++ apps/app/src/server/util/createApiRouter.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express, { type Router } from 'express';
 import CertifyOrigin from '~/server/middlewares/certify-origin';
 
-function createApiRouter() {
-  const r = express.Router();
-  r.use(CertifyOrigin);
-  return r;
+function createApiRouter(): Router {
+  const router = express.Router();
+  router.use(CertifyOrigin);
+  return router;
 }
 
 export {
