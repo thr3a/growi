c10cd0a263ed8dce778330cb1b1055f845535e11
Yuki Takei <yuki@weseek.co.jp>
2025-12-08 12:22:45 +0000

    clean code

--- apps/app/src/client/components/Sidebar/SidebarHeaderReloadButton.tsx
+++ apps/app/src/client/components/Sidebar/SidebarHeaderReloadButton.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-
 type Props = {
   onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 };
 
-export const SidebarHeaderReloadButton = ({ onClick }: Props) => {
+export const SidebarHeaderReloadButton = ({ onClick }: Props): JSX.Element => {
 
   return (
     <button type="button" className="btn btn-sm ms-auto py-0 grw-btn-reload" onClick={onClick}>
