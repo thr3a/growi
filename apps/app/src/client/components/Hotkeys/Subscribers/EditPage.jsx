3023e6235b5c3b0de3a1a5664a219c9e0461e9f5
Shun Miyazawa <m0zurillex@gmail.com>
2025-12-11 09:05:21 +0000

    feat: implement page editing functionality with shortcut key on empty page

--- apps/app/src/client/components/Hotkeys/Subscribers/EditPage.jsx
+++ apps/app/src/client/components/Hotkeys/Subscribers/EditPage.jsx
@@ -2,29 +2,42 @@ import { useEffect } from 'react';
 
 import PropTypes from 'prop-types';
 
-import { useIsEditable } from '~/states/page';
-import { EditorMode, useEditorMode } from '~/states/ui/editor';
+import { useStartEditing } from '~/client/services/use-start-editing';
+import { useCurrentPathname } from '~/states/global';
+import { useIsEditable, useCurrentPagePath } from '~/states/page';
+import { useEditorMode } from '~/states/ui/editor';
 
 const EditPage = (props) => {
   const isEditable = useIsEditable();
   const { setEditorMode } = useEditorMode();
+  const startEditing = useStartEditing();
+  const currentPagePath = useCurrentPagePath();
+  const currentPathname = useCurrentPathname();
+  const path = currentPagePath ?? currentPathname;
 
   // setup effect
   useEffect(() => {
-    if (!isEditable) {
-      return;
-    }
+    (async () => {
+      if (!isEditable) {
+        return;
+      }
 
-    // ignore when dom that has 'modal in' classes exists
-    if (document.getElementsByClassName('modal in').length > 0) {
-      return;
-    }
+      // ignore when dom that has 'modal in' classes exists
+      if (document.getElementsByClassName('modal in').length > 0) {
+        return;
+      }
 
-    setEditorMode(EditorMode.Editor);
+      try {
+        await startEditing(path);
+      }
+      catch (err) {
+      //
+      }
 
-    // remove this
-    props.onDeleteRender(this);
-  }, [isEditable, props, setEditorMode]);
+      // remove this
+      props.onDeleteRender(this);
+    })();
+  }, [startEditing, isEditable, path, props, setEditorMode]);
 
   return null;
 };
